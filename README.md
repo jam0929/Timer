c1934bed21d505d1cc91fa51e87380463fbbe6c3
James <jam0929@gmail.com>
2016-07-27 15:02:44 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+#Javascript Timer
+## 사용법 - 주기적으로 실행되어야 하는 function 개발
+
+#TODO
+## 모듈화 작업
---
+++ Timer.js
@@ -0,0 +1,26 @@
+var Timer = function(config) {
+    var _config = config ? config : {
+        initialTime : 60 * 60 * 1000,
+        interval : 1000
+    };
+
+    var currentTime = _config.initialTime;
+
+    setInterval(function() {
+        document.getElementsByClassName('timer')[0].innerHTML = msToTime(currentTime);
+        currentTime = currentTime - _config.interval;
+    }, 1000);
+
+    function msToTime(ms) {
+        var milliseconds = parseInt((ms%1000)/100)
+            , seconds = parseInt((ms/1000)%60)
+            , minutes = parseInt((ms/(1000*60))%60)
+            , hours = parseInt((ms/(1000*60*60))%24);
+
+        hours = (hours < 10) ? "0" + hours : hours;
+        minutes = (minutes < 10) ? "0" + minutes : minutes;
+        seconds = (seconds < 10) ? "0" + seconds : seconds;
+
+        return hours + ":" + minutes + ":" + seconds;
+    }
+};
